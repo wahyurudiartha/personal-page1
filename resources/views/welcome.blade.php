eb245babef66732a7dd61fc9f7166f468289a01b
wahyurudiartha <wahyurudiartha67@gmail.com>
2021-10-01 10:18:42 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+	  <div style="background-color: rgb(84, 123, 207)" class="container-fluid py-5">
+		  <div class="container">
+			  <div class="row">
+				  <div class="col-12">
+					  <h1 class="text-center">HAI</h1>
+				  </div>
+			  </div>
+			  <div class="row">
+				  <div class="col-6">
+					  <p class="text-end" >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos fugit quibusdam veritatis consectetur aperiam, explicabo esse atque sint cum perferendis quis nihil tempora quisquam molestiae adipisci unde. Nobis, eaque architecto.</p>
+				  </div>
+				  <div class="col-6">
+					<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos fugit quibusdam veritatis consectetur aperiam, explicabo esse atque sint cum perferendis quis nihil tempora quisquam molestiae adipisci unde. Nobis, eaque architecto.</p>
+				</div>
+			  </div>
+		  </div>
+	  </div>
+
+	<div class="container-fluid py-5">
+		<div class="container">
+			<div class="row">
+				<div class="col-4 offset-2">
+					<img class="img-fluid" src="/img/bggg.png" alt="">
+					<h5 class="text-center mt-3">Hai</h5>
+					<p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic veniam consequuntur repudiandae accusantium quis placeat natus ut totam repellendus, libero dignissimos! Eveniet enim error deleniti. Vitae repellat sit saepe eum!</p>
+				</div>
+				<div class="col-4">
+					<img class="img-fluid" src="/img/bggg.png" alt="">
+					<h5 class="text-center mt-3">Hai</h5>
+					<p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic veniam consequuntur repudiandae accusantium quis placeat natus ut totam repellendus, libero dignissimos! Eveniet enim error deleniti. Vitae repellat sit saepe eum!</p>
+				</div>
+			</div>
+
+			<div class="row mt-3">
+				<div class="col-4">
+					<img class="img-fluid" src="/img/bggg.png" alt="">
+					<h5 class="text-center mt-3">Hai</h5>
+					<p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic veniam consequuntur repudiandae accusantium quis placeat natus ut totam repellendus, libero dignissimos! Eveniet enim error deleniti. Vitae repellat sit saepe eum!</p>
+				</div>
+				<div class="col-4">
+					<img class="img-fluid" src="/img/bggg.png" alt="">
+					<h5 class="text-center mt-3">Hai</h5>
+					<p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic veniam consequuntur repudiandae accusantium quis placeat natus ut totam repellendus, libero dignissimos! Eveniet enim error deleniti. Vitae repellat sit saepe eum!</p>
+				</div>
+				<div class="col-4">
+					<img class="img-fluid" src="/img/bggg.png" alt="">
+					<h5 class="text-center mt-3">Hai</h5>
+					<p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic veniam consequuntur repudiandae accusantium quis placeat natus ut totam repellendus, libero dignissimos! Eveniet enim error deleniti. Vitae repellat sit saepe eum!</p>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-4 offset-4">
+					<img class="img-fluid" src="/img/bggg.png" alt="">
+					<h5 class="text-center mt-3">Hai</h5>
+					<p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic veniam consequuntur repudiandae accusantium quis placeat natus ut totam repellendus, libero dignissimos! Eveniet enim error deleniti. Vitae repellat sit saepe eum!</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
